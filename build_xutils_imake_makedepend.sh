5441b6d3e1b8febeeedd6c0ac42cdf6f18d653f2
Luke Pickering <picker24@msu.edu>
2019-02-28 16:10:20 -0600

    build_xutils_imake_makedepend.sh now takes cli args to control which package is built.

--- build_xutils_imake_makedepend.sh
+++ build_xutils_imake_makedepend.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-mkdir -p xorg/imake
-cd xorg/imake
+if [ "${1}" == "1" ] || [ "${2}" == "1" ]; then
+
+mkdir -p xorg
+cd xorg
+
+if [ "${1}" == "1" ]; then
+
+mkdir imake
+cd imake
 
 wget https://www.x.org/archive/individual/util/imake-1.0.7.tar.gz
 tar -zxvf imake-1.0.7.tar.gz
@@ -18,6 +25,10 @@ echo -e "#!/bin/bash\nexport PATH=$(readlink -f build/Linux/bin):\${PATH}" > set
 
 cd ../
 
+fi
+
+if [ "${2}" == "1" ]; then
+
 mkdir makedepend
 cd makedepend
 
@@ -33,9 +44,21 @@ make install
 
 cd ..
 
-echo -e "#!/bin/bash\nexport PATH=$(readlink -f build/Linux/bin):\${PATH}" > setup.sh
+echo "#!/bin/bash\nexport PATH=$(readlink -f build/Linux/bin):\${PATH}" > setup.sh
+
+fi
 
 cd ..
 
-echo -e "#!/bin/bash\nsource $(readlink -f imake/setup.sh)\nsource $(readlink -f makedepend/setup.sh)" > setup.sh
+echo -e '#!/bin/bash' > xorg_utils_setup.sh
 
+if [ "${2}" == "1" ]; then
+  echo "source $(readlink -f imake/setup.sh)" >> xorg_utils_setup.sh
+fi
+
+if [ "${2}" == "1" ]; then
+  echo "source $(readlink -f makedepend/setup.sh)" >> xorg_utils_setup.sh
+fi
+
+
+fi
